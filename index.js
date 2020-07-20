348306dbb08b306f2446f81a4c2676144ec3ff35
Octavian Regatun <octavian.regatun@outlook.com>
2020-07-20 17:23:23 +0300

    setup workflow

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
